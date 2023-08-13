662bcc0de6a4d4b59b674442aff9f6faf3094792
Curtis Hu <curtisjhu@berkeley.edu>
2023-08-12 20:47:29 -0700

    Template

---
+++ README.md
@@ -0,0 +1,16 @@
+# Template For SFML
+- cross-platform
+- opens a bare minimum window
+
+```
+mkdir build
+cmake ..
+```
+Unix:
+```
+make
+```
+Win32:
+```
+mingw32-make
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,18 @@
-// #include <SFML/Window.hpp>
-
-// int main() {
-// 	sf::Window window(sf::VideoMode(800, 600), "Traffic Simulation");
-
-// 	while (window.isOpen())
-// 	{
-// 		sf::Event event;
-// 		while (window.pollEvent(event))
-// 		{
-// 			if (event.type == sf::Event::Closed)
-// 				window.close();
-// 		}
-// 	}
-
-// 	return 0;
-// }
-
+#include <SFML/Window.hpp>
 #include <iostream>
+
 int main() {
-	std::cout<<"HELLO WORLD"<<std::endl;
+	sf::Window window(sf::VideoMode(800, 600), "Traffic Simulation");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+
+	return 0;
 }
